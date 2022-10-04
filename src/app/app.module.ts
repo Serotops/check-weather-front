1c059ecc25b62050b5faee170989ae0aa45fb1d1
Spark <sparkismad@gmail.com>
2022-10-04 18:31:12 +0200

    Add toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderToolbarComponent } from './header-toolbar/header-toolbar.component';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderToolbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/header-toolbar/header-toolbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+
+@Component({
+  selector: 'app-header-toolbar',
+  templateUrl: './header-toolbar.component.html',
+  styleUrls: ['./header-toolbar.component.css']
+})
+export class HeaderToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
